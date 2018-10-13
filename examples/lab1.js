58e8ca32d739426d87bdb5ccbcca3880d491a7be
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-10-13 00:29:06 -0300

    refactoring

--- examples/lab1.js
+++ examples/lab1.js
@@ -2,10 +2,25 @@
 
 const moduleTest = require("../lib");
 
+var context = {};
+context.getConfig = function(){
+    return {
+        db_url : "mongodb://localhost:27017",
+        db_username : "",
+        db_password : "",
+        db_name: "security",
+        db_collection_name: "user",
+        db_fieldPassword: "password",
+        db_fieldsReturn : ["name","email","roles"],
+        security_hash_algorithm : "SHA1", //SHA1, SHA256, SHA512
+        security_hash_salt : ""
+    };
+}
+
 var message1 = {};
 message1.username = "admin";
 message1.password = "123";
-moduleTest(message1).then(function(result){
+moduleTest(message1, context).then(function(result){
     console.log("sucess! ", result);
 }, function(err){
     console.log("err! ", err);
--- test/test.js
+++ test/test.js
@@ -3,10 +3,25 @@
 const assert = require("assert");
 const moduleTest = require("../");
 
+var context = {};
+context.getConfig = function(){
+    return {
+        db_url : "mongodb://localhost:27017",
+        db_username : "",
+        db_password : "",
+        db_name: "security",
+        db_collection_name: "user",
+        db_fieldPassword: "password",
+        db_fieldsReturn : ["name","email","roles"],
+        security_hash_algorithm : "SHA1", //SHA1, SHA256, SHA512
+        security_hash_salt : ""
+    };
+}
+
 var message1 = {};
 message1.username = "admin";
 message1.password = "123";
-moduleTest(message1).then(function(result){
+moduleTest(message1, context).then(function(result){
     assert.strictEqual(result.name, "admin");
 }, function(err){
     assert.strictEqual(err, null);
