cbb0fa3b729115ae57344d1bed55aede27169107
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-10-14 20:11:31 -0300

    refactoring

--- examples/lab1.js
+++ examples/lab1.js
@@ -1,14 +1,11 @@
 "use strict";
 
 const moduleTest = require("../lib");
+const driver = require("@functions-io-modules/datasource.driver.mongo");
 
 var context = {};
 context.getConfig = function(){
     return {
-        db_url : "mongodb://localhost:27017",
-        db_username : "",
-        db_password : "",
-        db_name: "security",
         db_collection_name: "user",
         db_fieldPassword: "password",
         db_fieldsReturn : ["name","email","roles"],
@@ -17,11 +14,20 @@ context.getConfig = function(){
     };
 }
 
+context.getDataSource = function(){
+    let config = {};
+    config.url = "mongodb://localhost:27017";
+    config.dataBaseName = "security";
+    return driver.getDataSource(config);
+}
+
 var message1 = {};
 message1.username = "admin";
 message1.password = "123";
 moduleTest(message1, context).then(function(result){
     console.log("sucess! ", result);
+    driver.close();
 }, function(err){
     console.log("err! ", err);
+    driver.close();
 })
--- test/test.js
+++ test/test.js
@@ -3,13 +3,11 @@
 const assert = require("assert");
 const moduleTest = require("../");
 
+const driver = require("@functions-io-modules/datasource.driver.mongo");
+
 var context = {};
 context.getConfig = function(){
     return {
-        db_url : "mongodb://localhost:27017",
-        db_username : "",
-        db_password : "",
-        db_name: "security",
         db_collection_name: "user",
         db_fieldPassword: "password",
         db_fieldsReturn : ["name","email","roles"],
@@ -18,11 +16,20 @@ context.getConfig = function(){
     };
 }
 
+context.getDataSource = function(){
+    let config = {};
+    config.url = "mongodb://localhost:27017";
+    config.dataBaseName = "security";
+    return driver.getDataSource(config);
+}
+
 var message1 = {};
 message1.username = "admin";
 message1.password = "123";
 moduleTest(message1, context).then(function(result){
     assert.strictEqual(result.name, "admin");
+    driver.close();
 }, function(err){
     assert.strictEqual(err, null);
+    driver.close();
 })
